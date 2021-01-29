60582b52e8d34d1a79c307600c07a01b128f16ea
Sheik Barese <caiovictor.pcb@gmail.com>
2021-01-29 15:31:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-usuario = str(input("MEU USUARIO: "))
-senha = str(input("MINHA SENHA: "))
-perfil = str(input("PERFIL: "))
 
 
 #abrir a pagina do instagram no chrome
@@ -18,48 +15,89 @@ option.add_argument('--window-size=1920,1080')
 option.add_argument('--start-maximized') 
 #option.add_argument('--headless')
     
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
-
-
-# ver ou nao o chrome abrindo
-
-pagina = driver.get(url_insta) #abre a pagina
 
 # esperar o conteudo da pagina carregar
 time.sleep(10)
 # processo de entrar pelo facebook com minhas credenciais
-click_enter_by_facebook = driver.find_element_by_xpath('//html//body//div[1]//section//main//article//div[2]//div[1]//div//form//div[1]//div[5]//button').click()
 
 
 
-time.sleep(3)
+def open_chrome():
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+    driver.get(url_insta)
 
-fill_username = driver.find_element_by_xpath('//html//body//div[1]//div[3]//div[1]//div//div//div[2]//div[1]//form//div//div[1]//input').send_keys(usuario + Keys.TAB)
-
-time.sleep(2)
-
-fill_password = driver.find_element_by_xpath('//html//body//div[1]//div[3]//div[1]//div//div//div[2]//div[1]//form//div//div[2]//input').send_keys(senha + Keys.ENTER)
-
-time.sleep(10)
-
-#desativar notificoes ao entrar no instagram
-no_notifications = driver.find_element_by_xpath('//html//body//div[4]//div//div//div//div[3]//button[2]').click()
+def notifications_search():
+    driver.find_element_by_xpath('//html//body//div[4]//div//div//div//div[3]//button[2]').click()
 
 
-#pesquisar perfil que eu quiser
-search_user = driver.find_element_by_xpath('//html//body//div[1]//section//nav//div[2]//div//div//div[2]//input').send_keys(perfil)
+    #pesquisar perfil que eu quiser
+    driver.find_element_by_xpath('//html//body//div[1]//section//nav//div[2]//div//div//div[2]//input').send_keys(perfil)
 
-time.sleep(3)
+    time.sleep(3)
 
-#clicar no primeiro perfil achado
-search_user = driver.find_element_by_xpath('//html//body//div[1]//section//nav//div[2]//div//div//div[2]//div[4]//div//a[1]').click()
+    #clicar no primeiro perfil achado
+    driver.find_element_by_xpath('//html//body//div[1]//section//nav//div[2]//div//div//div[2]//div[4]//div//a[1]').click()
 
-#extrair numero de seguidores
-time.sleep(10)
-numero_seguidores = driver.find_element_by_xpath('//html//body//div[1]//section//main//div//header//section//ul//li[2]//a//span').text
-print(numero_seguidores)
+    #extrair numero de seguidores
+    time.sleep(10)
+    seguidores = driver.find_element_by_xpath('//html//body//div[1]//section//main//div//header//section//ul//li[2]//a//span').text
+    print(f'O número de seguidores de {perfil} é {seguidores}.')
+
+    time.sleep(10)
+    driver.quit()
+
+def enter_by_facebook():
+    open_chrome()
+    time.sleep(6)
+    driver.find_element_by_xpath('//html//body//div[1]//section//main//article//div[2]//div[1]//div//form//div[1]//div[5]//button').click()
+                                
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//html//body//div[1]//div[3]//div[1]//div//div//div[2]//div[1]//form//div//div[1]//input').send_keys(usuario + Keys.TAB)
+
+    time.sleep(2)
+
+    driver.find_element_by_xpath('//html//body//div[1]//div[3]//div[1]//div//div//div[2]//div[1]//form//div//div[2]//input').send_keys(senha + Keys.ENTER)
+
+    time.sleep(10)
+    notifications_search()
+    #desativar notificoes ao entrar no instagram
 
 
-time.sleep(5)
 
 
+def enter_by_instagram():
+    open_chrome()
+    time.sleep(8)
+    driver.find_element_by_xpath('//html//body//div[1]//section//main//article//div[2]//div[1]//div//form//div//div[1]//div//label//input').click().send_keys(usuario + Keys.TAB)
+    driver.find_element_by_xpath('//html//body//div[1]//section//main//article//div[2]//div[1]//div//form//div//div[2]//div//label//input').send_keys(senha + Keys.ENTER)
+    time.sleep(5)
+    notifications_search()
+
+def pedir_credenciais():
+    global usuario, senha, perfil
+    usuario = str(input(f"usuário/email do {metodo}: "))
+    senha = str(input(f"senha do {metodo}: "))
+    perfil = str(input("perfil : "))
+    
+
+def menu():
+    print('+---------------------------+')
+    print("1.Entrar pelo facebook")
+    print("2.Entrar pelo instagram.")
+    print("+---------------------------+")
+    selection = int(input("Escolha uma opção: "))
+    if selection == 1:
+        global metodo
+        metodo = 'facebook'
+        pedir_credenciais()
+        enter_by_facebook()
+    elif selection == 2:
+        metodo = 'instagram'
+        pedir_credenciais()
+        enter_by_instagram()
+    else:
+        menu()
+
+menu()
